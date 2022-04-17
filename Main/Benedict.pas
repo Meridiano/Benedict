dc0b2a17456b10112ffe1fcfc0f49286bfcc0669
Meridiano <megametallist@gmail.com>
2022-04-17 08:27:20 +0300

    Add files via upload

--- Main/Benedict.pas
+++ Main/Benedict.pas
@@ -1,4 +1,4 @@
-program Benedict;
+﻿program Benedict;
 
 const FFName = '01_01.txt';
 const FSName = '01_02.txt';
@@ -11,8 +11,7 @@ begin
   var NMax, wNum: integer;
   writeln('How many? With numbers (0/1)?');
   readln(NMax, wNum);
-  if (NMax < 1) or ((wNum <> 0) and (wNum <> 1)) then
-  begin
+  if (NMax < 1) or ((wNum <> 0) and (wNum <> 1)) then begin
     write('Error!');
     readln;
     exit;
@@ -22,8 +21,7 @@ begin
   AssignFile(FFFile, FFName);
   reset(FFFile);
   var FFCount: integer = 0;
-  while not eof(FFFile) do
-  begin
+  while not eof(FFFile) do begin
     readln(FFFile, FFArray[FFCount]);
     FFCount := FFCount + 1;
   end;
@@ -33,8 +31,7 @@ begin
   AssignFile(FSFile, FSName);
   reset(FSFile);
   var FSCount: integer = 0;
-  while not eof(FSFile) do
-  begin
+  while not eof(FSFile) do begin
     readln(FSFile, FSArray[FSCount]);
     FSCount := FSCount + 1;
   end;
@@ -44,8 +41,7 @@ begin
   AssignFile(SFFile, SFName);
   reset(SFFile);
   var SFCount: integer = 0;
-  while not eof(SFFile) do
-  begin
+  while not eof(SFFile) do begin
     readln(SFFile, SFArray[SFCount]);
     SFCount := SFCount + 1;
   end;
@@ -55,8 +51,7 @@ begin
   AssignFile(SSFile, SSName);
   reset(SSFile);
   var SSCount: integer = 0;
-  while not eof(SSFile) do
-  begin
+  while not eof(SSFile) do begin
     readln(SSFile, SSArray[SSCount]);
     SSCount := SSCount + 1;
   end;
@@ -72,30 +67,27 @@ begin
   var sNLength: integer;
   str(NMax, sNMax);
   sNMaxLength := sNMax.Length;
-  for var N := 1 to NMax do
-  begin
-    if (wNum = 1) then
-    begin
+  for var N := 1 to NMax do begin
+    if (wNum = 1) then begin
       str(N, sN);
       sNLength := sN.Length;
-      while sNLength < sNMaxLength do
-      begin
+      while sNLength < sNMaxLength do begin
         sN := '0' + sN;
         sNLength := sNLength + 1;
       end;
       write(OutputFile, sN, ') ');
     end;
     
-    Temp:=random(FFCount);
+    Temp := random(FFCount);
     write(OutputFile, FFArray[Temp]);
     
-    Temp:=random(FSCount);
+    Temp := random(FSCount);
     write(OutputFile, FSArray[Temp], ' ');
     
-    Temp:=random(SFCount);
+    Temp := random(SFCount);
     write(OutputFile, SFArray[Temp]);
     
-    Temp:=random(SSCount);
+    Temp := random(SSCount);
     writeln(OutputFile, SSArray[Temp]);
   end;
   
